16288718b2a325d6a15e7a663b47e9e4a3e55795
Mohammed Jamaluddin <mohammedjamaluddin5377@gmail.com>
2025-03-22 11:35:27 +0530

    Small change

--- app/classroom.jsx
+++ app/classroom.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import CircularProgress from "react-native-circular-progress-indicator";
 
 const Classroom = () => {
   return (
     <View className="flex-row flex-1 justify-center items-center">
       <Text className="text-3xl">Resources Page</Text>
+      <CircularProgress radius={25} value={100} duration={1000} />
     </View>
   );
 };
