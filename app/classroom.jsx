b43dbb958442217a5e811202bdf44a7143ae5645
Mohammed Jamaluddin <mohammedjamaluddin5377@gmail.com>
2025-03-22 13:11:26 +0530

    some changes

--- app/classroom.jsx
+++ app/classroom.jsx
@@ -5,7 +5,7 @@ import { StyleSheet, View, Text } from "react-native";
 const Classroom = () => {
   return (
     <View className="flex-row flex-1 justify-center items-center">
-      <Text className="text-3xl">Resources Page</Text>
+      <Text className="text-3xl">Classroom Page</Text>
     </View>
   );
 };
